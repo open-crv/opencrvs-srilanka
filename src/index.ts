f7dc63feb74bc594d9b9081855458a147593bfd2
Ahamed34 <ahamed.asif@pickme.lk>
2026-01-19 16:06:03 +0530

    added the new docker configs v9

--- src/index.ts
+++ src/index.ts
@@ -443,7 +443,9 @@ export async function createServer() {
     method: 'POST',
     path: '/event-registration',
     handler: async (request, h) => {
-      const url = env.isProd ? 'http://mosip-api:2024' : 'http://localhost:2024'
+      const url = env.isProd
+        ? 'https://api-internal.sdec.mosip.net'
+        : 'http://localhost:2024'
       const result = await verify({ url, request })
       const bundle = request.payload as fhir3.Bundle
 
@@ -635,7 +637,9 @@ export async function createServer() {
     method: 'POST',
     path: '/events/{event}/actions/sent-notification',
     handler: mosipRegistrationForReviewHandler({
-      url: env.isProd ? 'http://mosip-api:2024' : 'http://localhost:2024'
+      url: env.isProd
+        ? 'https://api-internal.sdec.mosip.net'
+        : 'http://localhost:2024'
     }),
     options: {
       tags: ['api', 'custom-event'],
@@ -647,7 +651,9 @@ export async function createServer() {
     method: 'POST',
     path: '/events/{event}/actions/sent-notification-for-review',
     handler: mosipRegistrationForReviewHandler({
-      url: env.isProd ? 'http://mosip-api:2024' : 'http://localhost:2024'
+      url: env.isProd
+        ? 'https://api-internal.sdec.mosip.net'
+        : 'http://localhost:2024'
     }),
     options: {
       tags: ['api', 'custom-event'],
@@ -660,7 +666,9 @@ export async function createServer() {
     method: 'POST',
     path: '/events/{event}/actions/sent-for-approval',
     handler: mosipRegistrationForApprovalHandler({
-      url: env.isProd ? 'http://mosip-api:2024' : 'http://localhost:2024'
+      url: env.isProd
+        ? 'https://api-internal.sdec.mosip.net'
+        : 'http://localhost:2024'
     }),
     options: {
       tags: ['api', 'custom-event'],
